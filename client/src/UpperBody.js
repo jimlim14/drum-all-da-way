b1ea6dc41bfe97fdfc42e7153bff38c2ee959772
Jim <limchorngjim@gmail.com>
2022-04-25 18:26:18 +0200

    commit upperBody to see if indentation working

--- client/src/UpperBody.js
+++ client/src/UpperBody.js
@@ -3,28 +3,30 @@ import './upperBody.css';
 //import { Link } from 'react-router-dom';
 
 export default function UpperBody() {
-	function handleGetStarted () {
-		return document
-			.getElementById('footer')
-			.scrollIntoView({ behavior: 'smooth', block: 'center' });
-	}
-	
-	return (
-		<section id='upper-body'>
-			<div className='upper-body-text-div'>
-				<h1 className='upper-body-h1'>
-					Learn to play drums no matter what levels you are in.
-				</h1>
-				<p className='upper-body-p'>
-					Beginners, Amateurs, Professional. Follow your own pace, pick an
-					instructor that suits you best.
-				</p>
-				{/* <Link to='/getstarted'>  */}
-				<button className='upper-body-btn' onClick={handleGetStarted}>
-					Get started
-				</button>
-				{/* </Link> */}
-			</div>
-		</section>
-	);
+  function handleGetStarted() {
+    return document
+      .getElementById('footer')
+      .scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
+
+  return (
+    <>
+      <section id='upper-body'>
+        <div className='upper-body-text-div'>
+          <h1 className='upper-body-h1'>
+            Learn to play drums no matter what levels you are in.
+          </h1>
+          <p className='upper-body-p'>
+            Beginners, Amateurs, Professional. Follow your own pace, pick an
+            instructor that suits you best.
+          </p>
+          {/* <Link to='/getstarted'>  */}
+          <button className='upper-body-btn' onClick={handleGetStarted}>
+            Get started
+          </button>
+          {/* </Link> */}
+        </div>
+      </section>
+    </>
+  );
 }
